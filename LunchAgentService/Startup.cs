dcee775e3c039f1dfd0e605a73098026a3771799
Juřička Tomáš <juricka@cngroup.dk>
2020-02-12 17:48:11 +0100

    Configure Startup, route in userController

--- LunchAgentService/Startup.cs
+++ LunchAgentService/Startup.cs
@@ -1,14 +1,20 @@
 ﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using LunchAgentService.Entities;
+using LunchAgentService.Helpers;
 using LunchAgentService.Services;
 using LunchAgentService.Services.DatabaseService;
 using LunchAgentService.Services.MachineLearningService;
+using LunchAgentService.Services.UserService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
@@ -28,6 +34,9 @@ namespace LunchAgentService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddMvc();
+
             var databaseService = new DatabaseService(Configuration["connectionString"], Configuration["databaseName"], Logger);
 
             LoadConfigFileIntoDatabase(Configuration, databaseService);
@@ -43,9 +52,45 @@ namespace LunchAgentService
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
 
-            services.AddCors();
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
 
-            services.AddMvc();
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = context =>
+                        {
+                            var userService = context.HttpContext.RequestServices.GetRequiredService<IUserService>();
+                            var userId = context.Principal.Identity.Name;
+                            var user = userService.GetById(userId);
+                            if (user == null)
+                            {
+                                // return unauthorized if user no longer exists
+                                context.Fail("Unauthorized");
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
 
         private void LoadConfigFileIntoDatabase(IConfiguration configuration, DatabaseService databaseService)
@@ -63,6 +108,14 @@ namespace LunchAgentService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .Build());
+            app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
@@ -78,16 +131,6 @@ namespace LunchAgentService
             {
                 app.UseExceptionHandler("/Error");
             }
-
-            app.UseStaticFiles();
-
-            app.UseAuthentication();
-
-            app.UseCors(
-                options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
-            );
-
-            app.UseMvc();
         }
     }
 }

821179741fa59b209eb1d6db4a7ae4a9af20e4bd
Tomáš Juřička <32677484+Tomas-Juri@users.noreply.github.com>
2021-07-15 14:21:18 +0200

    Fix (#6)
    * Add using
    
    * Revert "Replaced ILoger with Tracer (#2)"
    
    This reverts commit 7d02af5b7c05f4dd03943d8dbc1a91b359b320dd.

--- LunchAgentService/Startup.cs
+++ LunchAgentService/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
@@ -14,11 +15,13 @@ namespace LunchAgentService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(ILogger<Startup> logger, IConfiguration configuration)
         {
+            Logger = logger;
             Configuration = configuration;
         }
 
+        public ILogger Logger { get; }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
